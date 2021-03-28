14ee39f100e069854ce05f7a5db079de21fba25b
Conner Martin <55250241+Martinc11@users.noreply.github.com>
2021-03-28 17:46:50 -0400

    Added CliView for integer only.

---
+++ ConsoleAdventureGame/view/CliView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+namespace ConsoleAdventureGame.view{
+    public class CliView{
+        
+        public void output(String message){
+            Console.WriteLine(message);
+        }
+
+        public int input(){
+            String input = Console.ReadLine();
+            bool isInt = false;
+            while (!isInt){
+                try{
+                    Int32.Parse(input);
+                    isInt = true;
+                } catch {
+                    output("Please enter an integer per the previous prompt!");
+                    input = Console.ReadLine();
+                }
+            }
+            return Int32.Parse(input);
+        }
+    }
+}
