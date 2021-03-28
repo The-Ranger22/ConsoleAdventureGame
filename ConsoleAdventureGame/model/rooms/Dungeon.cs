e5844a2234db0ccca4f71e05df3a1aa920aabdef
Levi Schanding <schandingl1@nku.edu>
2021-03-28 17:58:59 -0400

    prefabA now populates dungeon object w/ 3 weapons.

--- ConsoleAdventureGame/model/rooms/Dungeon.cs
+++ ConsoleAdventureGame/model/rooms/Dungeon.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections.Generic;
 using ConsoleAdventureGame.model.creatures;
 using ConsoleAdventureGame.model.items;
+using ConsoleAdventureGame.model.items.weapon;
 
 namespace ConsoleAdventureGame.model.rooms{
     public class Dungeon{
         public static int RoomCount{ get; private set; } = 0;
         public Room[] Rooms{ get; private set; }
 
-        Dungeon(){
+        public Dungeon(){
             prefabA();
         }
 
@@ -21,6 +22,13 @@ namespace ConsoleAdventureGame.model.rooms{
             Rooms[5] = new Room(RoomCount++, new []{4, 6, 7});
             Rooms[6] = new Room(RoomCount++, new []{5});
             Rooms[7] = new Room(RoomCount++, new []{5});
+
+            Rooms[2].Contents.AddFirst(new MeleeWeapon(EnMeleeWeapons.DAGGER));
+            Rooms[1].Contents.AddFirst(new MeleeWeapon(DamageType.BLUNT, new DamageRoll(3, 4), "The Mighty Mack",
+                "A four foot long mackerel ideal for bludgeon someone to death with.", 1));
+            Rooms[7].Contents.AddFirst(new MeleeWeapon(EnMeleeWeapons.GREATSWORD));
+            
+
         }
         
         
