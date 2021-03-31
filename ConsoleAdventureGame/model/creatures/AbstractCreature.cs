1393cabb36f581d376976a8b995f45602824aad6
Conner Martin <55250241+Martinc11@users.noreply.github.com>
2021-03-31 18:30:51 -0400

    Comments for AbstractCreature.cs

--- ConsoleAdventureGame/model/creatures/AbstractCreature.cs
+++ ConsoleAdventureGame/model/creatures/AbstractCreature.cs
@@ -5,6 +5,11 @@ using ConsoleAdventureGame.model.items.armor;
 using ConsoleAdventureGame.model.items.weapon;
 using ConsoleAdventureGame.model.rooms;
 
+/*
+ * Class Description: Abstract class representing common properties and behaviors of Creatures. Main functionality exists in the fight() method, where
+ * recursive calls continue until either the player or the NPC creature are dead. While some properties are used by the player exclusively, they were included to facilitate later enhancements to game.
+ */
+
 namespace ConsoleAdventureGame.model.creatures{
     public abstract class AbstractCreature{
         public int Health{ get; set; }
@@ -30,6 +35,13 @@ namespace ConsoleAdventureGame.model.creatures{
             Inventory.Add(Armor);
         }
 
+        /**
+         * Params: An Abstract Creature, either the Player or an NPC. 
+         * First attacks per turn and defense score are calculated based on the weapon's number of strikes + strength bonus and the Armor's Score + agility bonus respectively.
+         * As an NPC creature at this point cannot consume potions, those values will always default to the Armor and Weapons natural values during calculation.
+         * At the end of the fight, Strength Boost and Agility boost are reset
+         * to false, as a potion will only last one fight. 
+         */
         protected void Fight(AbstractCreature opponent){
             Random gen = new Random();
             // Should not effect non-player characters. Calculate bonus value based on presence of strength or agility boost. 
@@ -40,7 +52,7 @@ namespace ConsoleAdventureGame.model.creatures{
             if (Health > 0){ //if the player is still alive, attack
                 for (int i = 0; i < actualAttacksPerTurn; i++){
                     if ((gen.Next(20) + 1) > defenseScore){ //roll to hit
-                        opponent.TakeDamage((Weapon != null) ? Weapon.CalculateDamage() : new DamageRoll(1, 4).roll()); //roll damage. 
+                        opponent.TakeDamage((Weapon != null) ? Weapon.CalculateDamage() : new DamageRoll(1, 4).roll()); // if hit successful, roll damage. 
                     }
                 }
                 if (opponent.Health > 0){
@@ -52,6 +64,9 @@ namespace ConsoleAdventureGame.model.creatures{
             AgilityBoost = false;
         }
 
+        /**
+         * Removes the damage taken from the current creatures health. 
+         */
         private void TakeDamage(int damage){
             Health -= damage;
         }
